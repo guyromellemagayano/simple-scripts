6f5b81d95856c2c03866f8d732bfb8f36e714b2c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2017-09-07 21:38:12 +0800

    Add magic eight game

---
+++ pymagiceight.py
@@ -0,0 +1,17 @@
+import sys
+import random
+
+question = input('Ask the magic 8 ball a question (Press enter to start): ')
+answers = [
+	"You are awesome",
+	"You suck at anything you do", 
+	"Keep up the good work",
+	"You're immoral",
+	"You're not normal anymore",
+	"No one loves you anymore",
+	"So many pretentious people",
+	"You rock",
+	"You're awesome",
+	"You're important to other people"
+]
+print(answers[random.randint(1, len(answers))])
