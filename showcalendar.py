653eb65f44cb012a2d23de3c928f67a834697966
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2017-09-07 23:41:17 +0800

    Update calendar

--- showcalendar.py
+++ showcalendar.py
@@ -4,10 +4,12 @@ Simple Calendar [v1.0]
 Displays a calendar with adjustable values to update the calendar of your preference
 """
 
+from datetime import datetime
 import calendar
 
+now = datetime.now()
 # Function to show calendar
 def showCalendar(yy, mm):
     return(calendar.month(yy, mm))
 
-print(showCalendar(2016, 2))
+print(showCalendar(now.year, now.month))
